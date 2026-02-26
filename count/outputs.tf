ee9b0b9841a30f74d8b5b3505b2ceb14d98336c1
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-26 15:18:04 +0530

    actions

--- count/outputs.tf
+++ count/outputs.tf
@@ -1,8 +1,8 @@
 # Output for ec2_instances
-/* output "roboshop-instances" {
+output "roboshop-instances" {
   value       = aws_instance.example
   description = "description"
-} */
+}
 
 output "fruits_names" {
   value        = var.fruits # Here, List allows duplicate values (Ordered, index based values[0],[1]....)
