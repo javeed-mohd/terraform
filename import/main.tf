4271af6c86f816dd98ccab7a6dc1fe1381279cee
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-24 21:48:40 +0530

    terraform

--- import/main.tf
+++ import/main.tf
@@ -1,12 +1,12 @@
 # Reverse process with infra, we create resource....
 resource "aws_instance" "import" {
-    instance_type           = "t3.micro"
-    ami                     = "ami-0220d79f3f480ecf5"
-    vpc_security_group_ids  = [
-        "sg-0bdbfe5495a912910"
-    ]
-    subnet_id               = "subnet-0388641d42d74eb47"
-    tags = {
-        Name = "import-demo-change"
-    }
+  instance_type = "t3.micro"
+  ami           = "ami-0220d79f3f480ecf5"
+  vpc_security_group_ids = [
+    "sg-0bdbfe5495a912910"
+  ]
+  subnet_id = "subnet-0388641d42d74eb47"
+  tags = {
+    Name = "import-demo-change"
+  }
 }
